8a4779fc035d7a73f79e2e6e2f7fed7041a03d54
Satalicious <enzooocf@gmail.com>
2023-05-26 23:44:02 +0200

    wip

--- components/chatRoom/ChatRoom.tsx
+++ components/chatRoom/ChatRoom.tsx
@@ -43,10 +43,12 @@ const ChatRoom = (props: IChatRoom) => {
     getMessagesByChatId(roomId);
     
     const newMessageHandler = (newMessage) => {
+      console.log(newMessage.created_on); //logs
       if (newMessage.chat_id === roomId) {
         setMessages((messages) => [...messages, newMessage]);
       }
     };
+    
     
     socket.on("new_message", newMessageHandler);
   
--- components/ui/message/Message.tsx
+++ components/ui/message/Message.tsx
@@ -4,10 +4,13 @@ import styles from './Message.module.scss'
 
 const Message = (props: IMessage) => {
     const {message, isSender,...rest} = props
+    let time = new Date(message.created_on);
+    let formattedTime = time.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
   return (
     <div {...rest} className={`${isSender ? styles.sender : styles.recipient } mb-8 rounded-2xl pl-4 pt-2 pb-3 pr-8 max-w-[500px] `} >
         <p className='text-5 font-bold'>{message.username}</p>
         <h3 className='break-word text-2xl'>{message.msg}</h3>
+        <p className='text-5 font-bold'>{formattedTime}</p>
     </div>
   )
 }
