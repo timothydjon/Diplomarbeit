5511bf8e7b81eb47f3cc8f624f7dfade2c95105b
Satalicious <enzooocf@gmail.com>
2023-05-29 20:09:18 +0200

    now showing Yesterday in timestamp

--- components/ui/roomTeaser/roomTeaser.tsx
+++ components/ui/roomTeaser/roomTeaser.tsx
@@ -8,27 +8,51 @@ const RoomTeaser = (props: IRoomTeaser) => {
   function formatDate(inputDate) {
     const date = new Date(inputDate);
     const now = new Date();
-  
+
     // Checking if the date is today
-    if (date.toDateString() === now.toDateString()) {
-        return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
+    if (isSameDay(date, now)) {
+      return date.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      });
     }
-  
+
+    // Checking if the date is yesterday
+    const yesterday = new Date(now);
+    yesterday.setDate(yesterday.getDate() - 1);
+    if (isSameDay(date, yesterday)) {
+      return 'Yesterday';
+    }
+
     // Checking if the date is from this week
-    const startOfWeek = now.getDate() - now.getDay(); 
-    const endOfWeek = startOfWeek + 6;
-    if (date.getDate() >= startOfWeek && date.getDate() <= endOfWeek) {
-        return date.toLocaleDateString('en-US', { weekday: 'short' });
+    if (isSameWeek(date, now)) {
+      return date.toLocaleDateString('en-US', { weekday: 'short' });
     }
-  
+
     // If not today or this week
     return date.toLocaleDateString('en-US', { day: 'numeric', month: 'short' });
   }
-  
+
+  function isSameDay(date1, date2) {
+    return (
+      date1.getDate() === date2.getDate() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getFullYear() === date2.getFullYear()
+    );
+  }
+
+  function isSameWeek(date1, date2) {
+    const diff = Math.abs(date1 - date2);
+    const oneDay = 1000 * 60 * 60 * 24;
+    const daysDiff = Math.floor(diff / oneDay);
+    return daysDiff <= 6 && date1.getDay() >= date2.getDay();
+  }
+
   const formattedDate = formatDate(room.last_message_sent);
 
   return (
-    <button onClick={()=>{setRoomId(roomId)}} className="w-full flex items-center justify-between px-4 hover:bg-gray-700 focus:outline-none mb-5">
+    <button onClick={() => { setRoomId(roomId) }} className="w-full flex items-center justify-between px-4 hover:bg-gray-700 focus:outline-none mb-5">
       <div className="flex items-center">
         <div className="h-16 w-16 rounded-full overflow-hidden">
           <img placeholder="blur" src="/assets/github_profilepic.png" className="w-full h-full object-cover" />
