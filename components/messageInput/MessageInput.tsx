140682f95fa5cd649bdfcdd5f07e6f41a858f879
Satalicious <enzooocf@gmail.com>
2023-06-15 13:43:39 +0200

    init

--- components/messageInput/MessageInput.tsx
+++ components/messageInput/MessageInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useContext } from 'react'
+import React, { useState, useRef, useContext, useCallback, useEffect, createRef } from 'react'
 import { socket } from '../../context/socket/SocketContext';
 import IMessageInput from './MessageInput.interface';
 import SendBtn from "../../assets/src/send.svg"
@@ -9,6 +9,7 @@ import Emoji from '../../assets/src/emojiSmile.svg'
 import { useOuterClick } from 'react-outer-click';
 import dynamic from 'next/dynamic';
 import { SessionContext } from '../../context/sessionContext';
+import { useDropzone } from 'react-dropzone';
 
 const Picker = dynamic(
   () => {
@@ -24,83 +25,120 @@ const MessageInput = (props: IMessageInput) => {
     const [msgText, setMsgText] = useState<string>("");
     const [msgType, setMsgType] = useState<number>(0);
     const [openEmoji, setOpenEmoji] = useState(false);
+    const fileInputRef = useRef(null); // Create a reference for the file input
+    const [imagePreviewUrl, setImagePreviewUrl] = useState(null);
     const el = useRef(null);
 
     const handleSendBtn = (content: string) => {
         if (msgText == "") { return; }
-        const newMessage = { msg: msgText, msg_type: 0, user_id: user_id, chat_id: chat_id, username: user.username };
+        //if img: /upload and set msg_type 1
+        const newMessage = { msg: msgText, msg_type: msgType, user_id: user_id, chat_id: chat_id, username: user.username };
         socket.emit('send_message', newMessage);
         setMsgText("");
     }
-    
 
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
         // handle Enter from keyboard
         if (e.key === 'Enter') {
             e.preventDefault();
             handleSendBtn("test");
         }
-    }
-    const handleEmojiBtn = (e: React.FormEvent<HTMLInputElement>) => {
-        e.preventDefault()
-        setOpenEmoji(true);
-    }
-
-    const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-        setMsgText(e.currentTarget.value);
-    }
+    };
+    
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMsgText(e.target.value);
+    };
+    
     const handleEmojiClick = (emojiData, event) => {
         setMsgText(prevMsgText => prevMsgText + emojiData.emoji);
     }
 
+    const dropzoneRef = createRef();
 
-// useOuterClick(el, (event) => {
-//     event.preventDefault();
-//     setOpenEmoji(false)
-// });
-useOuterClick(el, (event) => {
-    if (event.target !== document.querySelector('.message-input')) {
-        setOpenEmoji(false);
-    }
-});
+    const handleClipClick = (event) => {
+        if(fileInputRef.current)
+        fileInputRef.current.click();
+    };
+
+    useOuterClick(el, (event) => {
+        if (event.target !== document.querySelector('.message-input')) {
+            setOpenEmoji(false);
+        }
+    });
+
+    const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
+        if (acceptedFiles.length > 0) {
+            console.log(acceptedFiles[0].name);
+
+            // Create image preview URL
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImagePreviewUrl(reader.result);
+            };
+            reader.readAsDataURL(acceptedFiles[0]);
+        } else {
+            // Handle rejected files here
+            console.log('Rejected files:', rejectedFiles);
+        }
+    }, []);
+
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop,
+        noClick: false,
+        accept: {'image/*': []},
+    });
+    const { getRootProps: rootPros } = useDropzone({
+        onDrop,
+        noClick: true,
+        accept: {'image/*': []},
+    });
+
+    useEffect(() => {
+        setImagePreviewUrl(null);
+        setMsgText("");
+    }, [chat_id])
+    
     return (
         <div className='w-full' {...rest}>
-            <div className="flex w-full items-center p-2 relative">
-                        {
+            <div className="flex w-full items-center p-2 relative" {...rootPros()}>
+            {
                             openEmoji && <div ref={el} className='absolute top-[-450px] left-0'>
                                 <Picker onEmojiClick={handleEmojiClick}  />
                                 </div>
                         }
                 <div className="overflow-hidden w-full bg-brown rounded-lg flex items-center p-1">
                     <div className="py-1 px-3">
-                    <button
-                        onClick={()=>{setOpenEmoji(true)}}>
-                        <Emoji />
-                    </button>
-                        
+                        <button onClick={() => { setOpenEmoji(true) }}>
+                            <Emoji />
+                        </button>
                     </div>
-                    <input
-                        type="text"
-                        className="message-input w-full bg-brown text-3xl text-white placeholder-white outline-none py-2 pl-6"
-                        placeholder="Message"
-                        value={msgText}
-                        onChange={handleChange}
-                        onKeyDown={handleKeyDown}
-                    />
-                    <div className="py-1 px-5">
-                        <Clip />
+                    <div className="flex items-center flex-1">
+                        <textarea
+                            className="message-input flex-1 bg-brown text-3xl text-white placeholder-white outline-none py-2 pl-6 pr-10 resize-none"
+                            placeholder="Message"
+                            value={msgText}
+                            onChange={handleChange}
+                            onKeyDown={handleKeyDown}
+                            rows={1}
+                        />
+                        {imagePreviewUrl && <img src={imagePreviewUrl} alt="preview" style={{ maxHeight: '100px', maxWidth: '100px' }} />}
+                    </div>
+                    <div className="py-1 px-5" {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <button onClick={handleClipClick}>
+                            <Clip />
+                        </button>
                     </div>
                 </div>
                 <div className="bg-purple ml-4 p-4 rounded-full flex items-center justify-center">
-                    <button
-                        onClick={() => { handleSendBtn("test") }}>
-                        {msgText == "" && false ? <Microphone /> : <SendBtn />}
+                    <button onClick={() => { handleSendBtn("test") }}>
+                        {msgText === "" && false ? <Microphone /> : <SendBtn />}
                     </button>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default MessageInput
 
