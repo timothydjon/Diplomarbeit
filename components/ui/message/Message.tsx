574fc8eaf1cce433850965d9715b4adbecce1114
Satalicious <enzooocf@gmail.com>
2023-06-22 12:20:33 +0200

    .

--- components/ui/message/Message.tsx
+++ components/ui/message/Message.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IMessage from './Message.interface';
 import styles from './Message.module.scss';
 
@@ -6,6 +6,9 @@ const Message = (props: IMessage) => {
     const { message, isSender, ...rest } = props;
     let time = new Date(message.created_on);
     let formattedTime = time.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
+
+    // State to manage modal visibility
+    const [showModal, setShowModal] = useState(false);
 
     return (
         <div
@@ -17,8 +20,31 @@ const Message = (props: IMessage) => {
                 <p className='text-sm text-gray-600'>{formattedTime}</p>
             </div>
             {message.msg_type === 1 ? (
-                // Display image
-                <img src={message.msg} alt="Uploaded content" className="my-2"/>
+                // Display image with onClick to show modal
+                <div>
+                    <img 
+                        src={message.msg}
+                        alt="Uploaded content"
+                        className="my-2 cursor-pointer"
+                        onClick={() => setShowModal(true)}
+                    />
+
+                    {showModal && (
+                        <div className="fixed z-10 inset-0 overflow-y-auto" onClick={() => setShowModal(false)}>
+                            <div className="flex items-center justify-center min-h-screen">
+                                <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                                    <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                                </div>
+
+                                <div className="bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all" onClick={(e) => e.stopPropagation()}>
+                                    <div className="relative">
+                                        <img src={message.msg} alt="Uploaded content" style={{maxWidth: '1200px', maxHeight: '700px'}} className="m-4"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
             ) : (
                 // Display text message
                 <h3 className='break-word text-lg my-2'>{message.msg}</h3>
